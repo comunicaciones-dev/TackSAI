f8976e623eab8b1971da52572639246094df3bbb
comunicaciones-dev <comunicaciones@prismapro.cl>
2025-09-16 10:32:22 -0300

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { DashboardPage } from '@/components/dashboard-page';
+import { Toaster } from "@/components/ui/toaster";
+
+export default function Home() {
+  return (
+    <>
+      <DashboardPage />
+      <Toaster />
+    </>
+  );
+}
